cdbe697723bf2f609cfe6a212c71e2d0b8420f77
truong.hua <truong.hua@youthdev.net>
2021-10-07 01:02:52 +0700

    There is no need to check for django compaitibiity because we mostly base on current compatible version of MySQL driver

--- django_tidb/__init__.py
+++ django_tidb/__init__.py
@@ -1,9 +1,3 @@
-# Check Django compatibility before other imports which may fail if the
-# wrong version of Django is installed.
 import pkg_resources
 
-from .utils import check_django_compatability
-
 __version__ = pkg_resources.get_distribution("django-tidb").version
-
-check_django_compatability()
